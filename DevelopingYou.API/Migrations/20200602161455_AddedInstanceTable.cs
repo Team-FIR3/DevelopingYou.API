ebb1dd88839728a9c4e896ce19deffafac7e33ee
Francesco <francescodtroiano@gmail.com>
2020-06-02 11:15:51 -0500

    Migration, team fire the best

---
+++ DevelopingYou.API/Migrations/20200602161455_AddedInstanceTable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevelopingYou.API.Migrations
+{
+    public partial class AddedInstanceTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Goals",
+                table: "Goals");
+
+            migrationBuilder.RenameTable(
+                name: "Goals",
+                newName: "Goal");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Goal",
+                table: "Goal",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Instance",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GoalId = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instance", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Instance_Goal_GoalId",
+                        column: x => x.GoalId,
+                        principalTable: "Goal",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Instance_GoalId",
+                table: "Instance",
+                column: "GoalId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Instance");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Goal",
+                table: "Goal");
+
+            migrationBuilder.RenameTable(
+                name: "Goal",
+                newName: "Goals");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Goals",
+                table: "Goals",
+                column: "Id");
+        }
+    }
+}
