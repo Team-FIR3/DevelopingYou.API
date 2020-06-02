c71cc28eb9b8f253d035a79248a082d1932d8106
Francesco <francescodtroiano@gmail.com>
2020-06-02 09:51:25 -0500

    InstanceDTO completed

--- DevelopingYou.API/Models/DTOs/InstanceDTO.cs
+++ DevelopingYou.API/Models/DTOs/InstanceDTO.cs
@@ -7,5 +7,14 @@ namespace DevelopingYou.API.Models.DTOs
 {
     public class InstanceDTO
     {
+        public int GoalId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Comment { get; set; }
     }
 }
