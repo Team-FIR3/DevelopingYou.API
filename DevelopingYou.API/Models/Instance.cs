ea4fb83113b57caffdf4dcfc76b9d524d5032414
Francesco <francescodtroiano@gmail.com>
2020-06-02 09:41:43 -0500

    Instance Model complete

--- DevelopingYou.API/Models/Instance.cs
+++ DevelopingYou.API/Models/Instance.cs
@@ -7,5 +7,19 @@ namespace DevelopingYou.API.Models
 {
     public class Instance
     {
+        public int GoalId { get; set; }
+
+        [Display(Name = "Date")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Date Goal Started")]
+        public DateTime StartTime { get; set; }
+
+        [Display(Name = "Ending Goal Time")]
+        public DateTime EndTime { get; set; }
+
+        [Display(Name = "Comments")]
+        public string Comment { get; set; }
+
     }
 }
