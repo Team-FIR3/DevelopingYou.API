86fd49fc2d0de80d16e0263dab14f4f1e36c3bc2
cnickels21 <cnickels_21@yahoo.com>
2020-07-06 14:14:33 -0500

    user dependency injection wired up

---
+++ DevelopingYou.API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevelopingYou.API.Data.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevelopingYou.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+    }
+}
