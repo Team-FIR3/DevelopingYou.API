42b48e8b3bcc9ac66fcf95ff875e8da18c03404b
cnickels21 <cnickels_21@yahoo.com>
2020-07-07 15:24:54 -0500

    updated userid type migration

---
+++ DevelopingYou.API/Migrations/20200707202417_GoalsUserIdUpdated.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevelopingYou.API.Migrations
+{
+    public partial class GoalsUserIdUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Goal",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Goal",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UserId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Goal_UserId",
+                table: "Goal",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Goal_AspNetUsers_UserId",
+                table: "Goal",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Goal_AspNetUsers_UserId",
+                table: "Goal");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Goal_UserId",
+                table: "Goal");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Goal",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Goal",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UserId",
+                value: 0);
+        }
+    }
+}
--- DevelopingYou.API/Models/Goal.cs
+++ DevelopingYou.API/Models/Goal.cs
@@ -8,7 +8,7 @@ namespace DevelopingYou.API.Models
     {
         public int Id { get; set; }
 
-        public int UserId { get; set; }
+        public string UserId { get; set; }
 
         [Required]
         public string Title { get; set; }
